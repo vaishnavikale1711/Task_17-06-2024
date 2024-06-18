960643fa4c4ad443fd34548968f1fb4e0ddde427
vaishnavikale1711 <vaishnavikale1711@gmail.com>
2024-06-18 17:41:12 +0530

    Task

---
+++ src/com/java/task1.java
@@ -0,0 +1,26 @@
+package com.java;
+
+public class task1 {
+	
+	//Java program execution starts from main ()method
+			public static void main(String[] args) {
+				// main()method starts from here
+				String Name = "Vaishnavi kale ";
+				System.out.println(Name);//prints name 
+				String Graduation = "Bachelor of Technology in Information Technology"	;
+				System.out.println(Graduation);// prints name of gradution degree
+				int yearofpassing = 2024;
+				System.out.println(yearofpassing);//prints year of passing from college
+				boolean isgraduated = true;
+				System.out.println(isgraduated);// prints true if gradution pass, prints false if gradution failed
+				String Address = "Sambhajinagar";
+				System.out.println(Address);// prints from where you are means address
+			}//main() method ends here
+
+
+	}
+	
+	
+	
+
+
---
+++ src/com/java/task2.java
@@ -0,0 +1,21 @@
+package com.java;
+
+public class task2 {
+	
+	public static void main(String[] args) {
+		// main()method starts from here
+		
+       //S.I. = (P*R*T)100*/         
+		
+		int P = 56000;  //   borrowed amount
+		
+		int R = 4;     // Rate  
+		
+		int T = 14;    // Time for 
+		
+		int SimpleInterest = (P*R*T)/100; // formula  
+		
+				System.out.println(SimpleInterest);// prints the simple interest
+				
+	 } //main() method ends here
+}
